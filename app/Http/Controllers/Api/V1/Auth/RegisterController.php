3324ffb4033718e9a9aab2a62526b6940e60f884
noah.scharrenberg <noah.scharrenberg@worldofcontent.com>
2021-09-07 09:48:09 +0200

    Improve Register flow

--- app/Http/Controllers/Api/V1/Auth/RegisterController.php
+++ app/Http/Controllers/Api/V1/Auth/RegisterController.php
@@ -4,29 +4,43 @@ namespace App\Http\Controllers\Api\V1\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class RegisterController extends Controller
 {
     public function register(Request $request)
     {
-        $this->validate($request, [
+        $validator = Validator::make($request->all(), [
             'name' => 'required',
             'email' => 'required|email|unique:users',
-            'password' => 'required|min:6',
+            'password' => 'required|min:8|confirmed',
         ]);
 
-        $user = User::create([
-            'name' => $request->name,
-            'email' => $request->email,
-            'password' => bcrypt($request->password),
-        ]);
+        if ($validator->fails()) {
+            return response()->json([
+                'message' => 'Email or Password not filled in',
+                'fields' => $validator->errors(),
+            ], 422);
+        }
 
-        $token = $user->createToken('galleme')->accessToken;
+        try {
+            $user = User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => bcrypt($request->password),
+            ]);
 
-        return response()->json([
-            'user' => $user,
-            'token' => $token
-        ], 201);
+            return response()->json([
+                'user' => $user,
+                'message' => 'Account has been successfully created',
+                'token' => $user->createToken('gallemeToken')->accessToken,
+            ], 201);
+        } catch(Exception $e) {
+            return response()->json([
+                'message' => 'Failed to create account'
+            ], 500);
+        }
     }
 }
