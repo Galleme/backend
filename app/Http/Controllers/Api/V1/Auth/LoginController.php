53d29a6906b17e8f17115b5235dcad0911894716
noah.scharrenberg <noah.scharrenberg@worldofcontent.com>
2021-09-07 09:43:00 +0200

    Improve Login flow

--- app/Http/Controllers/Api/V1/Auth/LoginController.php
+++ app/Http/Controllers/Api/V1/Auth/LoginController.php
@@ -4,25 +4,39 @@ namespace App\Http\Controllers\Api\V1\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 
 class LoginController extends Controller
 {
     public function login(Request $request)
     {
-        if (auth()->attempt([
-            'email' => $request->email,
-            'password' => $request->password,
-        ])) {
-            $token = auth()->user()->createToken('galleme')->accessToken;
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
 
+        if ($validator->fails()) {
             return response()->json([
-                'user' => auth()->user(),
-                'token' => $token
+                'message' => 'Email or Password not filled in',
+                'fields' => $validator->errors(),
+            ], 422);
+        }
+
+        $credentials = $request->only('email', 'password');
+
+        if (Auth::attempt($credentials)) {
+            $user = Auth::user();
+            
+            return response()->json([
+                'user' => $user,
+                'message' => 'Logged in',
+                'token' => $user->createToken('gallemeToken')->accessToken,
             ], 200);
         }
 
         return response()->json([
-            'error' => 'Incorrect email or password'
+            'message' => 'Incorrect email or password',
         ], 401);
     }
 }
